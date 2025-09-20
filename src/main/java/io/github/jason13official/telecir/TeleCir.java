131b3c76295a68af052d91f62886e745b248ea3e
jason13official <jason13official@gmail.com>
2025-09-20 16:02:02 -0400

    feat: server entrypoint

--- src/main/java/io/github/jason13official/telecir/TeleCir.java
+++ src/main/java/io/github/jason13official/telecir/TeleCir.java
@@ -1,12 +1,16 @@
 package io.github.jason13official.telecir;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
 
 public class TeleCir implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
 		Constants.LOG.info("Began common initialization.");
+		ServerTickEvents.START_SERVER_TICK.register(TeleCirServer::init);
+		ServerLifecycleEvents.SERVER_STOPPING.register(TeleCirServer::dereference);
 		Constants.LOG.info("Ended common initialization.");
 	}
 }
---
+++ src/main/java/io/github/jason13official/telecir/TeleCirServer.java
@@ -0,0 +1,54 @@
+package io.github.jason13official.telecir;
+
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.Level;
+
+public class TeleCirServer {
+
+  private static TeleCirServer instance;
+  private static boolean initialized;
+
+  private final MinecraftServer server;
+
+  public TeleCirServer(final MinecraftServer server) {
+
+    this.server = server;
+
+    ServerLevel level = this.server.getLevel(Level.OVERWORLD);
+
+    if (level == null) {
+      throw new IllegalStateException(
+          this.getClass().getSimpleName() + " was unable to retrieve Level.OVERWORLD");
+    }
+
+    Constants.LOG.info("Retrieving data storage from level {}: ", level.dimension());
+    Constants.LOG.info(level.getDataStorage().toString());
+  }
+
+  public static TeleCirServer getInstance() {
+    return instance;
+  }
+
+  public static MinecraftServer getServer() {
+    return getInstance().server;
+  }
+
+  public static void init(final MinecraftServer server) {
+    if (!initialized) {
+      Constants.LOG.info("Began server initialization.");
+      TeleCirServer.instance = new TeleCirServer(server);
+      initialized = true;
+      Constants.LOG.info("Ended server initialization.");
+    }
+  }
+
+  public static void dereference(final MinecraftServer server) {
+    if (initialized) {
+      Constants.LOG.info("Began server dereferencing.");
+      TeleCirServer.instance = null;
+      initialized = false;
+      Constants.LOG.info("Ended server dereferencing.");
+    }
+  }
+}
